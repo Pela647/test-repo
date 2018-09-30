7c27f6c065c0f56d2ef5af75cf46c5ac2206d93e
Pela647 <robel@engineer.com>
2018-09-29 22:45:50 -0400

    edit

---
+++ emoji.js
@@ -0,0 +1,96 @@
+angular.module('myApp', [])
+  .controller('myController', function ($scope) {
+
+
+    $scope.people = [{
+      name: 'laugh-cry',
+      code: String.fromCodePoint(0x1f602)
+    }, {
+      name: 'laugh-eye',
+      code: String.fromCodePoint(0x1f604)
+    }, {
+      name: 'laugh-smile',
+      code: String.fromCodePoint(0x1f603)
+    }, {
+      name: 'glasses',
+      code: String.fromCodePoint(0x1f9d0)
+    }, {
+      name: 'laugh-widetheeth',
+      code: String.fromCodePoint(0x1f600)
+    }, {
+      name: 'laugh-cry',
+      code: String.fromCodePoint(0x1f601)
+    }, {
+      name: 'g',
+      code: String.fromCodePoint(0x1f606)
+    }, {
+      name: 'dd',
+      code: String.fromCodePoint(0x1f605)
+    }, {
+      name: 'ff',
+      code: String.fromCodePoint(0x1f923)
+    }, {
+      name: 'laugh-eyes',
+      code: String.fromCodePoint(0x1f658)
+    }, {
+      name: 'laugh-eye',
+      code: String.fromCodePoint(0x1f648)
+    }, ];
+    $scope.flags = [{
+      name: 'laugh-cry',
+      code: String.fromCodePoint(0x1f602)
+    }, {
+      name: 'laugh-eye',
+      code: String.fromCodePoint(0x1f604)
+    }, {
+      name: 'laugh-smile',
+      code: String.fromCodePoint(0x1f603)
+    }, {
+      name: 'glasses',
+      code: String.fromCodePoint(0x1f9d0)
+    }, {
+      name: 'laugh-widetheeth',
+      code: String.fromCodePoint(0x1f600)
+    }, {
+      name: 'laugh-cry',
+      code: String.fromCodePoint(0x1f601)
+    }, {
+      name: 'g',
+      code: String.fromCodePoint(0x1f606)
+    }, {
+      name: 'dd',
+      code: String.fromCodePoint(0x1f605)
+    }, {
+      name: 'ff',
+      code: String.fromCodePoint(0x1f923)
+    }];
+    $scope.country = [{
+      name: 'laugh-cry',
+      code: String.fromCodePoint(0x1f602)
+    }, {
+      name: 'laugh-eye',
+      code: String.fromCodePoint(0x1f604)
+    }, {
+      name: 'laugh-smile',
+      code: String.fromCodePoint(0x1f603)
+    }, {
+      name: 'glasses',
+      code: String.fromCodePoint(0x1f9d0)
+    }, {
+      name: 'laugh-widetheeth',
+      code: String.fromCodePoint(0x1f600)
+    }, {
+      name: 'laugh-cry',
+      code: String.fromCodePoint(0x1f601)
+    }, {
+      name: 'g',
+      code: String.fromCodePoint(0x1f606)
+    }, {
+      name: 'dd',
+      code: String.fromCodePoint(0x1f605)
+    }, {
+      name: 'ff',
+      code: String.fromCodePoint(0x1f923)
+    }];
+
+  });
